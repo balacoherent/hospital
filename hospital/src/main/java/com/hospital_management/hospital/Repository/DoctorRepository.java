021f1b597b28638f9547840c3c6f4547245f94c0
balacoherent <86341262+balacoherent@users.noreply.github.com>
2021-09-04 22:07:43 +0530

    Delete hospital directory

--- hospital/src/main/java/com/hospital_management/hospital/Repository/DoctorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hospital_management.hospital.Repository;
-
-import com.hospital_management.hospital.Entity.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
-}
--- hospital/src/main/java/com/hospital_management/hospital/Repository/PatientRepositroy.java
+++
@@ -1,9 +0,0 @@
-package com.hospital_management.hospital.Repository;
-
-import com.hospital_management.hospital.Entity.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PatientRepositroy extends JpaRepository<Patient, Integer> {
-}
